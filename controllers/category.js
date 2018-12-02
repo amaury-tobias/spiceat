ce7942a01c2375568aca51ee07f85bd5bf0b5b62
Amaury Tobias Quiroz <amaury.tobiasqr@gmail.com>
2018-12-02 13:36:03 -0600

    recipe

--- controllers/category.js
+++ controllers/category.js
@@ -9,17 +9,44 @@ router.get('/category', async ctx => {
     ctx.body = categories
 })
 
+router.get('/categoryAdd', async ctx => {
+    /*RecipeModel.create({
+        name: 'Hamburguesa de pollo',
+        category: 'Hamburguesas',
+        details: [{
+            number: 1,
+            img: 'BurgerChkn1',
+            description: 'Compre pollo (que no este verde pls)'
+        },
+        {
+            number: 2,
+            img: 'BurgerChkn2',
+            description: 'Ponga el pollo entre los panes, (cocido, no sea barbaro)'
+        },
+        {
+            number: 3,
+            img: 'BurgerChkn3',
+            description: 'Agreguele unas verduritas, ¿Cuantas?, yo que sea pocas'
+        },
+        {
+            number: 4,
+            img: 'BurgerChkn4',
+            description: '¡Listo!, jamburguesa lista'
+        }]
+    })*/
+})
+
 router.get('/category/:name', async ctx => {
     let name = ctx.params.name
-    let recipes = await RecipeModel.find({ category: name})
+    let recipes = await RecipeModel.find({ category: name })
     ctx.body = recipes
 })
 
 router.get('/category/:name/:id', async ctx => {
     let name = ctx.params.name
     let id = ctx.params.id
-    let categories = await CategoryModel.find({})
-    ctx.body = { name, id }
+    let recipe = await RecipeModel.findById(id)
+    ctx.body = recipe
 })
 
 module.exports = router
--- controllers/root.js
+++ controllers/root.js
@@ -5,8 +5,8 @@ const router = require('koa-router')()
 router.get('/', ctx => {
     let links = [
         '<a href="/category">/category</a>',
-        '<a href="/category/Carnes">/category/:name</a>',
-        '<a href="/category/Carnes/example">/category/:name/:id</a>'
+        '<a href="/category/Hamburguesas">/category/:name</a>',
+        '<a href="/category/Hamburguesas/example">/category/:name/:id</a>'
     ]
 
     ctx.body = links.join('<br />')
